2a9fc5e09272daaae0303e2941d43d4c514dfa12
Valikhanov19 <abdukakhkhorvalikhanoff@gmail.com>
2022-11-30 14:16:51 +0300

    finished

---
+++ index.js
@@ -0,0 +1,10 @@
+//const birthYear = prompt ("Enter Your Birth Year")
+//console.log("Your age: " + (2022 - birthYear));
+//const num = prompt("enter number")
+//console.log(num >= -999 && num >=999)let a = prompt('Raqam kiriting')
+
+let a = prompt('write number')
+console.log(Math.trunc(a / 3600000), 'hour')
+console.log(Math.trunc(a / 60000), 'minutes')
+console.log(Math.trunc(a / 1000), 'seconds')
+console.log(a, 'millisekund')
